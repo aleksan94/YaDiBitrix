f7fa5599eee2c1a30aaf7b7d85b2ecfd07356fd5
Alexander Shuvalov <Alexander.Shuvalov@norbit.ru>
2019-03-27 12:00:01 +0300

    Fix 0.1.1b

--- local/components/YaDi/plyr/parameters.js
+++
@@ -1,7 +0,0 @@
-function GetToken(arParams) {
-	this.arParams = arParams;
-	let a = document.createElement('a');
-	a.text = "Получить токен";
-	a.href = "/bitrix/admin/yadi_get_token.php";
-	this.arParams.oCont.appendChild(a);
-}
